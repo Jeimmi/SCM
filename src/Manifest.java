4b7619455bc861c8723f2260773c2d79ee79c4ca
Jeimmi <jeimmi.gomez@gmail.com>
2017-05-07 15:59:36 -0700

    Adding Govi's changes on scm and manifest

--- src/Manifest.java
+++ src/Manifest.java
@@ -1,99 +1,120 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
+/**
+ * Created by Jeimmi Gomez and William Lee
+ *
+ * Author: Jeimmi Gomez and William Lee
+ *
+ * The Manifest class is taking care of collecting information
+ * to generate the manifest file
+ *
+ */
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 
 public class Manifest {
-	
-	private String mManifestTitle; //Title of namifest. --> Manifest_Date_Time
-	private String mUserCommand; //Is createRepo for now
-	private String mSourcePath; //The Source path of folder to copy.
-	private String mTargetPath; //The path of the repo
-	public ArrayList<String> mSourceFileNames; //Arraylist will hold all source file names in Repo
-	public ArrayList<String> mArtifactFileNames; //Arraylist will hold all artifact file names in Repo
-	public ArrayList<String> mSourcePaths; //Arraylist will hold all sourcepaths in repo
-	
-	/**
-	 * Manifest constructor
-	 * Create a manifest object
-	 * @param source The source folder path
-	 * @param target The target repo path
-	 * @param userCom The command the user gives
-	 */
-	public Manifest(String source,String target,String userCom){
-		//Get the date and time of the manifest creation
-		 mSourcePath = source;
-		 mTargetPath = target;
-		 mUserCommand = userCom;
 
-		//Instantiate arraylists to put sourcefiles, artifacts, and source paths in
-		mSourceFileNames = new ArrayList<String>();
-		mArtifactFileNames = new ArrayList<String>();
-		mSourcePaths = new ArrayList<String>();
-		
-		//Create the title by getting the date and time of when repo was created
-		DateFormat manifestDate = new SimpleDateFormat("yyyyMMdd");
-		DateFormat manifestTime = new SimpleDateFormat("HHmm");
-		Date dateObj = new Date();
-		mManifestTitle = "Manifest_" + manifestDate.format(dateObj) + "_" + manifestTime.format(dateObj);
-		
-	}
+    private String mManifestTitle; //Title of namifest. --> Manifest_Date_Time
+    private String mUserCommand; //Is createRepo for now
+    private File mSource; //The Source path of folder to copy.
+    private File mTarget; //The path of the repo
+
+    /**
+     * Manifest constructor
+     * Create a manifest object
+     * @param sourceFiles It contains stores the source and target directories and the files in the source directory (in that order)
+     * @param userCom The command the user gives
+     */
+    public Manifest(List sourceFiles,String userCom) throws IOException{
+        //Get the date and time of the manifest creation
+        mSource = (File) sourceFiles.get(0);
+        mTarget = (File) sourceFiles.get(1);
+        mUserCommand = userCom;
+
+        //Create the title by getting the date and time of when repo was created
+        DateFormat manifestDate = new SimpleDateFormat("yyyyMMdd");
+        DateFormat manifestTime = new SimpleDateFormat("HHmm");
+        Date dateObj = new Date();
+        mManifestTitle = "543-p1_GEL_" + manifestDate.format(dateObj) + "_" + manifestTime.format(dateObj) + "_" + userCom;
+
+        //Creating activity directory in the repository
+        File activityDirectory = new File(mTarget.getPath(), "Activity");
+        activityDirectory.mkdir();
+
+        //Put manifest file into archive folder
+        File manifestTextFile = new File (activityDirectory, getmManifestTitle() + ".txt");
+
+        //Write target & source information to manifest
+        writeToFile(manifestTextFile, "createRepo(sourceDirectory, targetDirectory)");
+        writeToFile(manifestTextFile, "  source directory path: " + mSource.getPath());
+        writeToFile(manifestTextFile, "  target directory path: " + mTarget.getPath());
+
+        //Write file & artifact information to manifest
+        File leaf, parent;
+        for(int i = 2, manifestSize = sourceFiles.size() - 1; i < manifestSize; i++) {
+            leaf = (File) sourceFiles.get(i);
+            parent = leaf.getParentFile();
+            writeToFile(manifestTextFile, "    " + parent.getName() + ", " + leaf.getName() + ", " + leaf.getPath());
+        }
+
+    }
 
 
-	public void userCommands(File manifest,String userCom) throws IOException{
-    	String command = null;
-		switch(userCom){
-		case "createRepo":
-			command = userCom+": "+this.getmSourcePath()+" "+this.getmTargetPath()+"\n";
-			break;	
-		}
+    /**
+     * Write to a text file
+     * @param file - The file to write to
+     * @param content - The string to write to given file
+     * @throws IOException
+     */
+    public static void writeToFile(File file, String content) throws IOException
+    {
+        StringBuilder newFile = new StringBuilder();
+        String edited = content;
+        newFile.append(edited);
+        FileWriter fstreamWrite = new FileWriter(file,true);
+        BufferedWriter out = new BufferedWriter(fstreamWrite);
+        out.write(newFile.toString());
+        out.newLine();
+        out.close();
+    }
+/*
+    public void userCommands(File manifest,String userCom) throws IOException{
+        String command = null;
+        switch(userCom){
+            case "createRepo":
+                command = userCom+": "+this.getmSourcePath()+" "+this.getmTargetPath()+"\n";
+                break;
+        }
+    }
+*/
 
-	}
-	
-	/**
-	 * addFileNames to member arraylists that will hold each file name in Manifest.
-	 * Will be called in the rescusive createRepo method in class SCM
-	 * @param sourcefileName
-	 * @param artifactFileName
-	 * @param sourcePath
-	 */
-	public void addFileNames(String sourcefileName, String artifactFileName, String sourcePath) {
-		//Add souce file, artifact file, and source path into arraylists
-		mSourceFileNames.add(sourcefileName);
-		mArtifactFileNames.add(artifactFileName);
-		mSourcePaths.add(sourcePath);
-	}
-	
-	public String getmManifestTitle() {
-		return mManifestTitle;
-	}
-	public void setmManifestTitle(String mManifestTitle) {
-		this.mManifestTitle = mManifestTitle;
-	}
-	public String getmUserCommand() {
-		return mUserCommand;
-	}
-	public void setmUserCommand(String mUserCommand) {
-		this.mUserCommand = mUserCommand;
-	}
-	public String getmSourcePath() {
-		return mSourcePath;
-	}
-	public void setmSourcePath(String mSourcePath) {
-		this.mSourcePath = mSourcePath;
-	}
-	public String getmTargetPath() {
-		return mTargetPath;
-	}
-	public void setmTargetPath(String mTargetPath) {
-		this.mTargetPath = mTargetPath;
-	}
-	
-	
+    public String getmManifestTitle() {
+        return mManifestTitle;
+    }
+
+    /*
+    public void setmManifestTitle(String mManifestTitle) {
+        this.mManifestTitle = mManifestTitle;
+    }
+    public String getmUserCommand() {
+        return mUserCommand;
+    }
+    public void setmUserCommand(String mUserCommand) {
+        this.mUserCommand = mUserCommand;
+    }
+    public String getmSourcePath() {
+        return mSourcePath;
+    }
+    public void setmSourcePath(String mSourcePath) {
+        this.mSourcePath = mSourcePath;
+    }
+    public String getmTargetPath() {
+        return mTargetPath;
+    }
+    public void setmTargetPath(String mTargetPath) {
+        this.mTargetPath = mTargetPath;
+    }
+*/
+
 }
--- src/scm.java
+++ src/scm.java
@@ -1,169 +1,97 @@
 /**
  * Created by LeonardoDaVinci on 2/21/2017.
  *
- * Author: Govinda Escobar
+ * Author: Govinda Escobar, Jeimmi Gomez, and William Lee
  *
  * This program copies the following to the target directory: the source directory (including the root),
  * all of its directories, & all of its files. For files, it creates a new directory with the file name &
  * stores the file in that directory
  *
- * Note: This works for at most one file per directory. Must implement checksum to handle multiple files.
- *
  */
-
 import java.io.*;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.security.MessageDigest;
 import java.util.Scanner;
 import java.nio.file.Paths;
+import java.util.*;
 
 public class scm {
-	
-	public void userCommands(File manifest){
-		System.out.println("1. Create new repo");
-    	Scanner s = new Scanner(System. in);
-    	int input = s.nextInt();
-		int userIn = input;
-		switch(userIn){
-		case 1:
-			break;
-		}
-	}
-	
-	/**
-	 * CheckSum method to create the checksum given any file
-	 * Algorithm used is prime weights multiplying by ASCII value and summing total
-	 * @param file - Any file to find checksum of
-	 * @return The check sum value and string of file name
-	 * @throws IOException
-	 */
-	public String checkSum(File file) throws IOException{
+    //It contains stores the source and target directories and the files in the source directory (in that order)
+    public List<File> sourceFiles = new ArrayList();
+
+    /**
+     * CheckSum method to create the checksum given any file
+     * Algorithm used is prime weights multiplying by ASCII value and summing total
+     * @param file - Any file to find checksum of
+     * @return The check sum value and string of file name
+     * @throws IOException
+     */
+
+    public String checkSum(File file) throws IOException{
+        int checkSumTotal = 0;
         String temp = new String(Files.readAllBytes(Paths.get(file.getPath())));
         int tempLength = temp.length();
-        int checkSumTotal = 0;
         int[] checkSumWeights = new int[] {1, 3, 11, 17}; //The prime weights for the checksum
-        
+
         //Loop through every character in text file
         for (int i = 0; i < tempLength; i++){
-        	//Cast each character into an int for the value. Mod the interator by 4 to figure out the prime weight
-            checkSumTotal +=  checkSumWeights[i % 4]* (int)temp.charAt(i); 
+            //Cast each character into an int for the value. Mod the interator by 4 to figure out the prime weight
+            checkSumTotal +=  checkSumWeights[i % 4]* (int)temp.charAt(i);
         }
         temp = file.getName(); //Get the file name
         return Integer.toString(checkSumTotal) + "." + Long.toString(file.length()) + temp.substring(temp.lastIndexOf("."));
     }
-	
-	/**
-	 * Recursive function to loop through tree of directory. 
-	 * @param sourceDirectory
-	 * @param destinationDirectory
-	 * @param manifestObject
-	 * @throws IOException
-	 */
-    private void createRepo(File sourceDirectory, File destinationDirectory, Manifest manifestObject) throws IOException {
-        destinationDirectory.mkdir();
-        if (sourceDirectory.isDirectory()) { //When it is a folder
+
+    /**
+     * Copies the source directory to the target directory. For files, it creates a new directory with the
+     * file name stores the file in that directory.
+     * @param sourceDirectory
+     * @param targetDirectory
+     * @throws IOException
+     */
+    private void createRepo(File sourceDirectory, File targetDirectory) throws IOException {
+        targetDirectory.mkdir();
+        if (sourceDirectory.isDirectory()) {
             // Returns an array of strings naming the files and directories in sourceDirectory
             String files[] = sourceDirectory.list();
 
-            // Copies files & directories to destinationDirectory
+            // Copies files & directories to targetDirectory
             for (String file : files)
             {
                 // Creates a new File instance from a parent abstract pathname and a child pathname string.
                 File sourceTemp = new File(sourceDirectory, file);
-                File destinationTemp = new File(destinationDirectory, file);
-                createRepo(sourceTemp, destinationTemp, manifestObject);
+                File targetTemp = new File(targetDirectory, file);
+                createRepo(sourceTemp, targetTemp);
             }
         }
-        else { //When it is a file
+        else {
             // Creates directory with file name
-            File leafDirectory = new File(destinationDirectory.toString(), checkSum(sourceDirectory));
+            File leafDirectory = new File(targetDirectory.toString(), checkSum(sourceDirectory));
             // Copies file into directory with its name
             Files.copy(sourceDirectory.toPath(), leafDirectory.toPath());
-            
+
             /*
              * Code to pass file name, artifact file name, and original path back to manifest
              */
-            String sourceFileName = sourceDirectory.getName();
-            String artifactFileName = leafDirectory.getName();
-            String sourcePath = sourceDirectory.getPath();
-            
-            manifestObject.addFileNames(sourceFileName, artifactFileName, sourcePath);
+            this.sourceFiles.add(leafDirectory);
         }
     }
-	
-    /**
-     * Write to a text file
-     * @param file - The file to write to
-     * @param content - The string to write to given file
-     * @throws IOException
-     */
-	public static void writeToFile(File file, String content) throws IOException
-	{
-	    StringBuilder newFile = new StringBuilder();
-	    String edited = content;
-	    newFile.append(edited);
-	    FileWriter fstreamWrite = new FileWriter(file,true);
-	    BufferedWriter out = new BufferedWriter(fstreamWrite);
-	    out.write(newFile.toString());
-	    out.newLine();
-	    out.close();
-	}
-    
+
 
     public static void main(String[] args) throws IOException
     {
-    	//Get input source and target path from user
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Input path for Windows using \\ and Apple using /");
-		System.out.println("windows ex: C:\\Users\\wills\\Desktop");
-		System.out.println("Apple ex: /Users/wills/Desktop");
-		System.out.println("Enter Path for Source Folder");
-		String sourcePath = scanner.nextLine();
-		System.out.println("Enter Path for Target Repo Folder");
-		String targetPath = scanner.nextLine();   	
-    	scm s = new scm();   	
-    	
-    	//sourcePath = "C:\\Users\\wills\\Desktop\\test_source";
-   
-        File sourceFolder = new File(sourcePath);
+        //Get input source and target path from user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Path for Source Folder");
+        File sourceDirectory = new File(scanner.nextLine());
+        System.out.println("Enter Path for Target Repo Folder");
+        File targetDirectory = new File(scanner.nextLine(), sourceDirectory.getName());
 
-        //targetPath = "C:\\Users\\wills\\Desktop\\test_destination" + sourceFolder.getName();
-        
-        File destinationFolder = new File(targetPath + sourceFolder.getName());
-        
-        
-        Manifest manifestObject = new Manifest(sourceFolder.getPath(), destinationFolder.getPath(),"createRepo");
+        scm s = new scm();
+        s.sourceFiles.add(sourceDirectory);
+        s.sourceFiles.add(targetDirectory);
 
-        s.createRepo(sourceFolder, destinationFolder,manifestObject);
-        
-        //Creating archived folder in the repository
-        File archiveFolder = new File(destinationFolder.getPath(), "Archive");
-        archiveFolder.mkdir();
-        
-        //Put manifest file into archive folder
-        File manifestTextFile = new File (archiveFolder, manifestObject.getmManifestTitle() + ".txt");
-        
-        manifestObject.userCommands(manifestTextFile, manifestObject.getmUserCommand());
-        writeToFile(manifestTextFile, "createRepo");
-        
-        
-        for(int i = 0; i < manifestObject.mArtifactFileNames.size() - 1; i++) {
-	    	String sourceFileName = manifestObject.mSourceFileNames.get(i);
-	    	String artifactFileName = manifestObject.mArtifactFileNames.get(i);
-	    	String sourcePathName = manifestObject.mSourcePaths.get(i);
+        s.createRepo(sourceDirectory, targetDirectory);
 
-	    	writeToFile(manifestTextFile, sourceFileName + ", " + artifactFileName + ", " + sourcePathName);
+        Manifest manifestObject = new Manifest(s.sourceFiles,"createRepo");
 
-	    	//mani.writeToFile(manifest, artifactFileName);
-	    	//mani.writeToFile(manifest, sourceFileName);
-
-	    	System.out.println(sourceFileName + ", " + artifactFileName + ", " + sourcePathName);
-	    }
-    	
     }
-
-
-}
